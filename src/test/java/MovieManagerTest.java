a5d392b8407929c6d959aff85f765874a7926164
Ekaterina Turkina <katrina.turkina@gmail.com>
2024-03-16 23:40:24 +0300

    last commit

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -4,29 +4,31 @@ import org.junit.jupiter.api.Test;
 public class MovieManagerTest {
 
     @Test
-    public void test1(){
+    public void test1() {
         MovieManager manager = new MovieManager();
 
         manager.addMovie("Film 1");
         manager.addMovie("Film 2");
         manager.addMovie("Film 3");
-        String[] expected = { "Film 1","Film 2","Film 3"};
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void test(){
+    public void test() {
         MovieManager manager = new MovieManager();
 
         manager.addMovie("Film 1");
         manager.addMovie("Film 2");
         manager.addMovie("Film 3");
-        String[] expected = { "Film 3","Film 2","Film 1"};
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void test2(){
+    public void test2() {
         MovieManager manager = new MovieManager();
 
         manager.addMovie("Film 1");
@@ -37,7 +39,7 @@ public class MovieManagerTest {
         manager.addMovie("Film 6");
         manager.addMovie("Film 7");
 
-        String[] expected = {"Film 7", "Film 6","Film 5","Film 4","Film 3"};
+        String[] expected = {"Film 7", "Film 6", "Film 5", "Film 4", "Film 3"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
